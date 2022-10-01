3baa438a5e9de056e5eb7b16b7d87429f1606eea
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-10-02 02:05:04 +0500

    deleted fonts files and directly imported from googlle fonts

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -3,8 +3,55 @@ import React from 'react';
 import { useState } from 'react';
 import { StyleSheet, Text, View , Button ,StatusBar, Image, TouchableOpacity} from 'react-native';
 import {Colors,Images,Fonts} from '../content'
+import {
+    useFonts,
+    Poppins_100Thin,
+    Poppins_100Thin_Italic,
+    Poppins_200ExtraLight,
+    Poppins_200ExtraLight_Italic,
+    Poppins_300Light,
+    Poppins_300Light_Italic,
+    Poppins_400Regular,
+    Poppins_400Regular_Italic,
+    Poppins_500Medium,
+    Poppins_500Medium_Italic,
+    Poppins_600SemiBold,
+    Poppins_600SemiBold_Italic,
+    Poppins_700Bold,
+    Poppins_700Bold_Italic,
+    Poppins_800ExtraBold,
+    Poppins_800ExtraBold_Italic,
+    Poppins_900Black,
+    Poppins_900Black_Italic,
+  } from '@expo-google-fonts/poppins';
+  
+  
+
+
 
 const SplashScreen = ()=> {
+
+
+    let [fontsLoaded] = useFonts({
+        Poppins_100Thin,
+        Poppins_100Thin_Italic,
+        Poppins_200ExtraLight,
+        Poppins_200ExtraLight_Italic,
+        Poppins_300Light,
+        Poppins_300Light_Italic,
+        Poppins_400Regular,
+        Poppins_400Regular_Italic,
+        Poppins_500Medium,
+        Poppins_500Medium_Italic,
+        Poppins_600SemiBold,
+        Poppins_600SemiBold_Italic,
+        Poppins_700Bold,
+        Poppins_700Bold_Italic,
+        Poppins_800ExtraBold,
+        Poppins_800ExtraBold_Italic,
+        Poppins_900Black,
+        Poppins_900Black_Italic,
+      });
 
   
   return (
@@ -19,7 +66,7 @@ const SplashScreen = ()=> {
         style={styles.image}/>
         <Text style={
             styles.titletext
-        }>SHAREMEAL</Text>
+        }>SHARE MEAL</Text>
       </View>
   );
 }
@@ -39,8 +86,9 @@ const styles = StyleSheet.create({
     titletext:
     {
         color:Colors.DEFAULT_WHITE,
-        fontSize:32,
-        fontFamily:Fonts.POPPINS_SEMI_BOLD
+        fontSize:28,
+        fontFamily:'Poppins_600SemiBold'
+
     }
 
   });
