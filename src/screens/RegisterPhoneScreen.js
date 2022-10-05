5c08f68f770dd6acaa63660ff342bd44f1866d91
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-10-05 22:01:13 +0500

    Imported and Loaded Fonts
    applied fonts logic fontsloaded&& for this component

--- src/screens/RegisterPhoneScreen.js
+++ src/screens/RegisterPhoneScreen.js
@@ -5,7 +5,7 @@ import {
   StatusBar,
   Image,
   TouchableOpacity,
-  TextInput
+  TextInput,
 } from "react-native";
 import React from "react";
 import { Colors } from "../content";
@@ -14,42 +14,53 @@ import { Display } from "../utils";
 import IonIcons from "react-native-vector-icons/Ionicons";
 import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 import { StaticImageService } from "../services";
+import {
+  useFonts, 
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
 
 const RegisterPhoneScreen = ({ navigation }) => {
+  let [fontsLoaded] = useFonts({
+    Poppins_500Medium,
+  });
   return (
-    <View style={styles.container}>
-      <StatusBar
-        barStyle={"dark-content"}
-        backgroundColor={Colors.DEFAULT_WHITE}
-        translucent
-      ></StatusBar>
+    fontsLoaded && (
+      <View style={styles.container}>
+        <StatusBar
+          barStyle={"dark-content"}
+          backgroundColor={Colors.DEFAULT_WHITE}
+          translucent></StatusBar>
 
-      <Separator height={StatusBar.currentHeight} />
-      <View style={styles.headerContainer}>
-        <IonIcons
-          name="chevron-back-outline"
-          size={30}
-          onPress={() => {
-            navigation.goBack();
-          }}
-        />
-        <Text style={styles.headertitle}>Login into Share Your Meal</Text>
-      </View>
-      <Separator height={StatusBar.currentHeight} />
-      <Text style={styles.content}>
-        Enter Your Registered Phone Number to Log in
-      </Text>
-      <View style={styles.inputsContainer}>
-        <TouchableOpacity style={styles.countryListContainer}>
-          <Image source={{uri: StaticImageService.getFlagIcon('CA')}} style={styles.flagIcon} />
-          <Text></Text>
-          <MaterialIcons/>
-        </TouchableOpacity>
-        <View style={styles.phoneInputContainer}>
-          <TextInput/>
+        <Separator height={StatusBar.currentHeight} />
+        <View style={styles.headerContainer}>
+          <IonIcons
+            name="chevron-back-outline"
+            size={30}
+            onPress={() => {
+              navigation.goBack();
+            }}
+          />
+          <Text style={styles.headertitle}>Login into Share Your Meal</Text>
+        </View>
+        <Separator height={StatusBar.currentHeight} />
+        <Text style={styles.content}>
+          Enter Your Registered Phone Number to Log in
+        </Text>
+        <View style={styles.inputsContainer}>
+          <TouchableOpacity style={styles.countryListContainer}>
+            <Image
+              source={{ uri: StaticImageService.getFlagIcon("CA") }}
+              style={styles.flagIcon}
+            />
+            <Text></Text>
+            <MaterialIcons />
+          </TouchableOpacity>
+          <View style={styles.phoneInputContainer}>
+            <TextInput />
+          </View>
         </View>
       </View>
-    </View>  
+    )
   );
 };
 
@@ -80,36 +91,36 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginHorizontal: 20,
   },
-  inputsContainer:{
-    flexDirection: 'row',
-    alignItems: 'center',
+  inputsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
     marginHorizontal: 20,
     marginVertical: 50,
   },
-  countryListContainer:{
+  countryListContainer: {
     backgroundColor: Colors.LIGHT_GREY,
     width: Display.setWidth(22),
     marginRight: 10,
     borderRadius: 8,
     height: Display.setHeight(6),
-    justifyContent:'space-evenly',
-    alignItems:'center',
+    justifyContent: "space-evenly",
+    alignItems: "center",
     borderWidth: 0.5,
-    borderColor:Colors.LIGHT_GREY2,
-    flexDirection:"row",
+    borderColor: Colors.LIGHT_GREY2,
+    flexDirection: "row",
   },
-  phoneInputContainer:{
+  phoneInputContainer: {
     backgroundColor: Colors.LIGHT_GREY,
     height: Display.setHeight(6),
     paddingHorizontal: 10,
     borderRadius: 8,
-    justifyContent:'center',
+    justifyContent: "center",
     borderWidth: 0.5,
-    borderColor:Colors.LIGHT_GREY2,
+    borderColor: Colors.LIGHT_GREY2,
     flex: 1,
   },
-  flagIcon:{
+  flagIcon: {
     height: 20,
     width: 20,
-  }
+  },
 });
