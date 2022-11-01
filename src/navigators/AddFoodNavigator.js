02701b3c8b88be8fb2a84c802cf37288301bb4fc
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2022-11-01 12:53:50 +0500

    Added Update Food Screen

--- src/navigators/AddFoodNavigator.js
+++ src/navigators/AddFoodNavigator.js
@@ -10,6 +10,7 @@ const AddFoodNavigator =()=> {
 
         <Stack.Navigator initialRouteName="AddFoodScreen" screenOptions={{ headerShown: false}}>
             <Stack.Screen name="AddFoodScreen" component={AddFoodScreen} />
+            <Stack.Screen name="UpdateFoodScreen" component={UpdateFoodScreen} />
             <Stack.Screen name="InsertFoodItemScreen" component={InsertFoodItemScreen} />
         </Stack.Navigator>
     );
