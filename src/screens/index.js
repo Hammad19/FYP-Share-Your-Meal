aad98c545144d96ad441a8d7798125d15721d36e
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2023-01-31 19:41:00 +0500

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,16 +1,29 @@
-import ForgotPasswordScreen from './ForgotPasswordScreen';
-import RegisterPhoneScreen from './RegisterPhoneScreen';
-import SigninScreen from './SigninScreen';
-import SignupScreen from './SignupScreen';
-import SplashScreen from './SplashScreen';
-import WelcomeScreen from './WelcomeScreen';
-import HomeScreen from './HomeScreen';
-import AddFoodScreen from './AddFoodScreen';
+import ForgotPasswordScreen from "./ForgotPasswordScreen";
+import RegisterPhoneScreen from "./RegisterPhoneScreen";
+import SigninScreen from "./SigninScreen";
+import SignupScreen from "./SignupScreen";
+import SplashScreen from "./SplashScreen";
+import WelcomeScreen from "./WelcomeScreen";
+import HomeScreen from "./HomeScreen";
+import AddFoodScreen from "./AddFoodScreen";
+import FilterScreen from "./FilterScreen";
+import FoodDetailScreen from "./FoodDetailScreen";
+import InsertFoodItemScreen from "./InsertFoodItemScreen";
+import UpdateFoodScreen from "./UpdateFoodScreen";
+import GoogleMapScreen from "./GoogleMapScreen";
 
-import FoodDetailScreen from './FoodDetailScreen';
-import InsertFoodItemScreen from './InsertFoodItemScreen';
-import UpdateFoodScreen from './UpdateFoodScreen';
-
-
-
-export {SplashScreen,WelcomeScreen,SigninScreen,SignupScreen, ForgotPasswordScreen,RegisterPhoneScreen,HomeScreen ,AddFoodScreen,InsertFoodItemScreen,FoodDetailScreen,UpdateFoodScreen};
+export {
+  SplashScreen,
+  WelcomeScreen,
+  SigninScreen,
+  SignupScreen,
+  ForgotPasswordScreen,
+  RegisterPhoneScreen,
+  HomeScreen,
+  AddFoodScreen,
+  InsertFoodItemScreen,
+  FoodDetailScreen,
+  UpdateFoodScreen,
+  FilterScreen,
+  GoogleMapScreen,
+};
