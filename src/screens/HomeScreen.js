b3a34ba568df15b6507f1831a92971478b6bd2d1
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2023-01-31 19:40:32 +0500

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -26,9 +26,17 @@ import Feather from "react-native-vector-icons/Feather";
 import { Display } from "../utils";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { useSelector, useDispatch } from "react-redux";
-import { getFood,getfoodbytype,getfoodforcharitableorganization } from "../store/slices/foodSlice";
+import {
+  getFood,
+  getfoodbytype,
+  getfoodforcharitableorganization,
+} from "../store/slices/foodSlice";
 import { setissharepage } from "../store/slices/userlistingSlice";
 import { useFocusEffect } from "@react-navigation/native";
+import Animated, { color } from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
+//import { black } from "react-native-paper/lib/typescript/styles/colors";
+
 const HomeScreen = ({ navigation }) => {
   const [foodType, setFoodType] = useState(true);
   const [delivery, setDelivery] = useState(true);
@@ -38,72 +46,87 @@ const HomeScreen = ({ navigation }) => {
     Poppins_600SemiBold,
   });
 
+    bs = React.createRef();
+    fall = new Animated.Value(1);
   //whenever the user comes in this page set issharepage to false
-  
-  
-
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
 
   const FetchFoodData = () => {
-
     let requestBody = {
       food_quantity: 5,
-      is_free: true
-    }
+      is_free: true,
+    };
 
     console.log(requestBody);
 
-    if(state.auth.user.accounttype === "User"){
-
+    if (state.auth.user.accounttype === "User") {
       dispatch(getFood());
-    }
-    else if(state.auth.user.accounttype === "Charitable Organization")
-
-    {
-
-      console.log("Hello in charitable")
+    } else if (state.auth.user.accounttype === "Charitable Organization") {
+      console.log("Hello in charitable");
 
       dispatch(getfoodforcharitableorganization(requestBody));
     }
   };
 
-
   const FetchFoodByType = () => {
-    if(foodType === "Free Food"){
+    if (foodType === "Free Food") {
       let requestBody = {
-        is_free: "true"
-      }
+        is_free: "true",
+      };
+      dispatch(getfoodbytype(requestBody));
+    } else if (foodType === "Paid Food") {
+      let requestBody = {
+        is_free: "false",
+      };
       dispatch(getfoodbytype(requestBody));
     }
-    else if(foodType === "Paid Food"){
-      let requestBody = {
-        is_free: "false"
-      }
-      dispatch(getfoodbytype(requestBody));
-    }
-  }
-    
+  };
+  renderHeader = () => (
+    <View style={styles.header}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle} />
+      </View>
+    </View>
+  );
+
+  renderInner = () => (
+    <View style={styles.panel}>
+      <TouchableOpacity style={styles.panelLocationButton} >
+        <Text style={styles.panelButtonTitle} onPress={() => navigation.navigate("GoogleMapScreen")}>+ Add location</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.panelButton}
+        onPress={() => bs.current.snapTo(0)}>
+        <Text style={styles.panelButtonTitle}>Confirm</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
   useEffect(() => {
     FetchFoodByType();
   }, [foodType]);
 
-
-    
   useFocusEffect(
     useCallback(() => {
       dispatch(setissharepage(false));
       FetchFoodData();
     }, [])
   );
-  
 
   return (
     fontsLoaded && (
-      
       <>
         <View style={styles.container}>
+        <BottomSheet 
+            ref={bs}
+            snapPoints={[0, 280]}
+            renderContent={renderInner}
+            renderHeader={renderHeader}
+            initialSnap={1}
+            callbackNode={fall}
+            enabledGestureInteraction={true}
+          />
           <StatusBar
             barStyle="light-content"
             backgroundColor="transparent"
@@ -153,55 +176,62 @@ const HomeScreen = ({ navigation }) => {
                 size={20}
                 color={Colors.DEFAULT_YELLOW}
                 style={{ marginRight: 10 }}
+                onPress={() => navigation.navigate("FilterScreen")}
               />
             </View>
-
-            {state.auth.user.accounttype === "User" && (<View
-              style={{
-                top: 20,
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-evenly",
-                width: Display.setWidth(100),
-              }}>
-              <TouchableOpacity
-                onPress={() => setFoodType("Free Food")}
-                style={styles.category()}>
-                <Ionicons
-                  name={foodType == "Free Food" ? "fast-food" : "fast-food-outline"}
-                  size={35}
-                  color={
-                    foodType == "Free Food"
-                      ? Colors.DEFAULT_WHITE
-                      : Colors.DEFAULT_GREY
-                  }
-                />
-                <Text style={styles.categoryText(foodType === "Free Food")}>
-                  FREE
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => setFoodType("Paid Food")}
-                style={styles.category()}>
-                <MaterialCommunityIcons
-                  name={foodType == "Paid Food" ? "food" : "food-outline"}
-                  size={35}
-                  color={
-                    foodType == "Paid Food"
-                      ? Colors.DEFAULT_WHITE
-                      : Colors.DEFAULT_GREY
-                  }
-                />
-                <Text style={styles.categoryText(foodType === "Paid Food")}>
-                  Order
-                </Text>
-              </TouchableOpacity>
-            </View>
+            {state.auth.user.accounttype === "User" && (
+              <View
+                style={{
+                  top: 20,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-evenly",
+                  width: Display.setWidth(100),
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => setFoodType("Free Food")}
+                  style={styles.category()}
+                >
+                  <Ionicons
+                    name={
+                      foodType == "Free Food"
+                        ? "fast-food"
+                        : "fast-food-outline"
+                    }
+                    size={35}
+                    color={
+                      foodType == "Free Food"
+                        ? Colors.DEFAULT_WHITE
+                        : Colors.DEFAULT_GREY
+                    }
+                  />
+                  <Text style={styles.categoryText(foodType === "Free Food")}>
+                    FREE
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => setFoodType("Paid Food")}
+                  style={styles.category()}
+                >
+                  <MaterialCommunityIcons
+                    name={foodType == "Paid Food" ? "food" : "food-outline"}
+                    size={35}
+                    color={
+                      foodType == "Paid Food"
+                        ? Colors.DEFAULT_WHITE
+                        : Colors.DEFAULT_GREY
+                    }
+                  />
+                  <Text style={styles.categoryText(foodType === "Paid Food")}>
+                    Order
+                  </Text>
+                </TouchableOpacity>
+              </View>
             )}
           </View>
-     
           <FlatList
-            style={{ flex: 1 ,zIndex:-1, paddingTop: 55}}
+            style={{ flex: 1, zIndex: -1, paddingTop: 55 }}
             data={state.food.foodlist}
             renderItem={({ item }) => <FoodItem post={item} />}
             //
@@ -307,4 +337,59 @@ const styles = StyleSheet.create({
     color: Colors.DEFAULT_WHITE,
     opacity: isActive ? 1 : 0.5,
   }),
+  header: {
+    backgroundColor: Colors.LIGHT_YELLOW,
+    shadowColor: '#333333',
+    shadowOffset: {width: -1, height: -3},
+    shadowRadius: 2,
+    shadowOpacity: 0.4,
+    // elevation: 5,
+    //paddingTop: 10,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height: 30,
+  },
+  panelHeader: {
+    alignItems: 'center',
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#00000040',
+    marginTop: 20,
+  },
+  panelButton: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: Colors.DEFAULT_YELLOW,
+    alignItems: 'center',
+    marginTop: 120,
+  },
+  panelLocationButton: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: Colors.DEFAULT_YELLOW,
+    borderLeftColor: 'black',
+    alignItems: 'center',
+  },
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  panel: {
+    padding: 10,
+    backgroundColor: Colors.LIGHT_YELLOW,
+    paddingTop: 20,
+    paddingBottom: 0,
+    height: 265,
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
+    // shadowColor: '#000000',
+    // shadowOffset: {width: 0, height: 0},
+    // shadowRadius: 5,
+    // shadowOpacity: 0.4,
+  },
+
 });
