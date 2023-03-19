36b0834532d39a836d9692d428d408d1ee879672
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2023-03-19 13:26:01 +0500

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,15 @@ import Navigators from "./src/navigators";
 import store from "./src/store/store";
 import { Provider } from "react-redux";
 
+const checkAutoLogin = async () => {
+  const user = await AsyncStorage.getItem("user");
+  if (user) {
+    //navigate to home
+  } else {
+    //navigate to login
+  }
+};
+
 export default () => (
   <Provider store={store}>
     <Navigators />
