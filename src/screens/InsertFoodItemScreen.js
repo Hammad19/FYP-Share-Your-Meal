4e599c81272404a02635ec8bbdd29e10f277b209
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2023-06-04 16:07:03 +0500

    Update InsertFoodItemScreen.js

--- src/screens/InsertFoodItemScreen.js
+++ src/screens/InsertFoodItemScreen.js
@@ -210,12 +210,12 @@ const InsertFoodItemScreen = ({ navigation }) => {
   };
 
   const handleonPress = () => {
-    // if (
-    //   state.auth.user.location_name == "" ||
-    //   state.auth.user.location_name == null
-    // ) {
-    //   Alert.alert("Please Add Your Location First");
-    // }
+    if (
+      state.auth.user.location_name == "" ||
+      state.auth.user.location_name == null
+    ) {
+      Alert.alert("Please Add Your Location First");
+    }
 
     if (foodType == "Free Food") {
       setfoodPrice("0");
@@ -278,7 +278,7 @@ const InsertFoodItemScreen = ({ navigation }) => {
         name: name,
       });
       axios
-        .post("http://192.168.10.25:8080/api/images/food-image", formData, {
+        .post("http://192.168.10.10:8080/api/images/food-image", formData, {
           headers: { "Content-Type": "multipart/form-data" },
         })
         .then((res) => {
@@ -315,7 +315,8 @@ const InsertFoodItemScreen = ({ navigation }) => {
         <StatusBar
           barStyle={"dark-content"}
           backgroundColor={Colors.DEFAULT_WHITE}
-          translucent></StatusBar>
+          translucent
+        ></StatusBar>
         <Separator height={StatusBar.currentHeight} />
         <View style={styles.headerContainer}>
           <IonIcons
@@ -330,7 +331,8 @@ const InsertFoodItemScreen = ({ navigation }) => {
         <View style={styles.inputImageContainer}>
           <TouchableHighlight
             underlayColor="rgba(0,0,0,0)"
-            onPress={() => setImage()}>
+            onPress={() => setImage()}
+          >
             {foodImage == null ? (
               <View style={styles.inputImageSubContainer}>
                 <IonIcons name="image-outline" size={50} color="grey" />
@@ -373,7 +375,8 @@ const InsertFoodItemScreen = ({ navigation }) => {
         <View
           style={
             isFieldInError("foodName") ? styles.error : styles.inputContainer
-          }>
+          }
+        >
           <View style={styles.inputSubContainer}>
             <IonIcons
               name="md-fast-food-outline"
@@ -405,7 +408,8 @@ const InsertFoodItemScreen = ({ navigation }) => {
             isFieldInError("foodDescription")
               ? styles.error
               : styles.inputContainer
-          }>
+          }
+        >
           <View style={styles.inputSubContainer}>
             <MaterialIcons
               name="details"
@@ -460,7 +464,8 @@ const InsertFoodItemScreen = ({ navigation }) => {
         <View
           style={
             isFieldInError("foodPrice") ? styles.error : styles.inputContainer
-          }>
+          }
+        >
           <View style={styles.inputSubContainer}>
             <IonIcons
               name="pricetags-outline"
