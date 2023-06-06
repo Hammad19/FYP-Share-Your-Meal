e908634acbdbdd51c140e40f4a8fbbba21c54da5
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2023-06-06 12:57:45 +0500

    Update UpdateFoodScreen.js

--- src/screens/UpdateFoodScreen.js
+++ src/screens/UpdateFoodScreen.js
@@ -276,7 +276,7 @@ const UpdateFoodScreen = ({ navigation, route }) => {
         name: name,
       });
       axios
-        .post("http://192.168.18.40:8080/api/images/food-image", formData, {
+        .post("http://192.168.43.79:8080/api/images/food-image", formData, {
           headers: { "Content-Type": "multipart/form-data" },
         })
         .then((response) => {
@@ -313,7 +313,8 @@ const UpdateFoodScreen = ({ navigation, route }) => {
         <StatusBar
           barStyle={"dark-content"}
           backgroundColor={Colors.DEFAULT_WHITE}
-          translucent></StatusBar>
+          translucent
+        ></StatusBar>
         <Separator height={StatusBar.currentHeight} />
         <View style={styles.headerContainer}>
           <IonIcons
@@ -328,7 +329,8 @@ const UpdateFoodScreen = ({ navigation, route }) => {
         <View style={styles.inputImageContainer}>
           <TouchableHighlight
             underlayColor="rgba(0,0,0,0)"
-            onPress={() => setImage()}>
+            onPress={() => setImage()}
+          >
             {foodImage == null ? (
               <View style={styles.inputImageSubContainer}>
                 <IonIcons name="image-outline" size={50} color="grey" />
@@ -346,7 +348,8 @@ const UpdateFoodScreen = ({ navigation, route }) => {
         <View
           style={
             isFieldInError("foodName") ? styles.error : styles.inputContainer
-          }>
+          }
+        >
           <View style={styles.inputSubContainer}>
             <IonIcons
               name="md-fast-food-outline"
@@ -374,7 +377,8 @@ const UpdateFoodScreen = ({ navigation, route }) => {
         <View
           style={
             isFieldInError("foodPrice") ? styles.error : styles.inputContainer
-          }>
+          }
+        >
           <View style={styles.inputSubContainer}>
             <IonIcons
               name="pricetags-outline"
@@ -406,7 +410,8 @@ const UpdateFoodScreen = ({ navigation, route }) => {
             isFieldInError("foodDescription")
               ? styles.error
               : styles.inputContainer
-          }>
+          }
+        >
           <View style={styles.inputSubContainer}>
             <MaterialIcons
               name="details"
@@ -512,7 +517,8 @@ const UpdateFoodScreen = ({ navigation, route }) => {
         ) : null}
         <TouchableOpacity
           onPress={handleUpdateFood}
-          style={styles.signinButton}>
+          style={styles.signinButton}
+        >
           <Text style={styles.signinButtonText}>Update Food</Text>
         </TouchableOpacity>
       </ScrollView>
